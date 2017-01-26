e988c65b1955d68546f2acbc9b2ac804ec52e199
taifatang <taifatang@hotmail.com>
2017-01-26 11:26:06 +0000

    Init Commit

---
+++ Asos.Hue.Api/Asos.Hue.Api.UnitTests/HueHubTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Should;
+
+namespace Asos.Hue.Api.UnitTests
+{
+    [TestFixture]
+   public  class HueHubTests
+    {
+        [Test]
+        public void Should_Return_An_Object_When_New()
+        {
+            var hueHub = new HueHub();
+            hueHub.ShouldNotBeNull();
+        }
+    }
+}
